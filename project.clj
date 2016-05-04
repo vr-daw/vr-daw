8b8ccce5a745812f96eca48166060428cd942d04
James <jmborden@gmail.com>
2016-05-03 22:17:00 -0500

    working red sphere with spacetime library

--- project.clj
+++ project.clj
@@ -1,11 +1,28 @@
 (defproject vr-daw "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
+  :description "A 3D Digital Audio Workspace"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.8.51"]]
-  :jvm-opts ^:replace ["-Xmx1g" "-server"]
-  :plugins [[lein-npm "0.6.1"]]
+                 [org.clojure/clojurescript "1.8.51"]
+                 [spacetime "0.2.0-SNAPSHOT"]]
+  ;; :jvm-opts ^:replace ["-Xmx1g" "-server"]
+  :plugins [[lein-npm "0.6.1"]
+            [lein-cljsbuild "1.1.2"]]
   :npm {:dependencies [[source-map-support "0.4.0"]]}
   :source-paths ["src" "target/classes"]
   :clean-targets ["out" "release"]
+  :cljsbuild {:builds [{:id "dev"
+                        :source-paths ["src"]
+                        :compiler {
+                                   :main vr-daw.core
+                                   :output-to "out/vr_daw.js"
+                                   :output-dir "out"
+                                   :optimizations :none
+                                   :pretty-print true
+                                   :source-map true}}
+                       {:id "release"
+                        :source-paths ["src"]
+                        :compiler {
+                                   :main vr-daw.core
+                                   :output-to "release/vr_daw.min.js"
+                                   :optimizations :advanced
+                                   :pretty-print false}}]}
   :target-path "target")
--- src/vr_daw/core.cljs
+++ src/vr_daw/core.cljs
@@ -1,9 +1,83 @@
 (ns vr-daw.core
-  (:require [clojure.browser.repl :as repl]))
+  (:require [clojure.browser.repl :as repl]
+            [spacetime.camera :refer [create-perspective-camera init-camera!]]
+            [spacetime.controls.original :as controls]
+            [spacetime.core :as spacetime]
+            [three]
+            ))
+
+(def request-id (atom nil))
 
 ;; (defonce conn
 ;;   (repl/connect "http://localhost:9000/repl"))
+(deftype Sphere
+    [geometry material mesh selected?]
+  Object
+  ;; specify the new color using hexadecial notation
+  (change-color [this color]
+    (.material.color.set this color)))
 
-(enable-console-print!)
+(defn sphere
+  "Create a sphere with initial coordinates x,y,z and radius."
+  [x y z radius]
+  (let [geometry (js/THREE.SphereGeometry. radius 32 16)
+        material (js/THREE.MeshBasicMaterial. (js-obj
+                                               "color" 0xFF0000
+                                               "side" js/THREE.DoubleSide))
+        mesh     (js/THREE.Mesh. geometry material)
+        selected? false]
+    (.position.set mesh x y z)
+    (Sphere. geometry material mesh selected?)))
 
-(println "Hello world!")
+;; because of the error
+;;Caused by: clojure.lang.ExceptionInfo: No such namespace: three, could not locate three.cljs, three.cljc, or Closure namespace "three" in file out/spacetime/camera.cljs
+;; we need to be properly packaging the foreign dependecies
+;; https://github.com/clojure/clojurescript/wiki/Packaging-Foreign-Dependencies
+;; however, in the future a cljsjs should be made for THREE and the supporting libs we use for it
+
+(defn ^:export init []
+  (let [scene (spacetime/create-scene)
+        camera (init-camera! (create-perspective-camera
+                              45
+                              (/ (.-innerWidth js/window)
+                                 (.-innerHeight js/window))
+                              0.1
+                              20000)
+                             scene
+                             [0 0 1300])
+        renderer (spacetime/create-renderer)
+        render (spacetime/render renderer scene camera)
+        container (-> js/document
+                      (.getElementById "ThreeJS"))
+        _ (spacetime/attach-renderer! renderer container)
+        _ (.addEventListener container "click"
+                             (fn [event]
+                               (-> js/document
+                                   .-body
+                                   ;; we should stub this out for webkit and moz
+                                   (.requestPointerLock))))
+        pointer-lock-controls (spacetime/pointer-lock-controls camera)
+        _ (.add scene (.getObject pointer-lock-controls))
+        _ (spacetime/pointer-lock-listener! js/document pointer-lock-controls)
+        skybox (let [skybox-geometry (spacetime/create-box-geometry 20000 20000 20000)
+                     skybox-material (spacetime/create-mesh-basic-material
+                                      (js-obj "color" 0x063140
+                                              "side" js/THREE.BackSide))]
+                 (js/THREE.Mesh. skybox-geometry skybox-material))
+        light  (js/THREE.HemisphereLight. 0xeeeeff 0x777788 0.75)
+        red-sphere (sphere 200 1 1 1000)
+        ]
+    ;;(.add scene skybox)
+    (.add scene (.-mesh red-sphere))
+    (spacetime/window-resize! renderer camera)
+    ;;(js/THREEx.WindowResize renderer camera)
+    ;;(.appendChild container (.-domElement stats))
+    ;;(.bindKey js/THREEx.FullScreen (js-obj "charCode" (.charCodeAt "m" 0)))
+    (spacetime/fullscreen!)
+    (spacetime/start-time-frame-loop (fn [delta-t]
+                             (do (render)
+                                 (controls/controls-handler camera)))
+                           request-id)
+    ;; add listeners for key events
+    (js/addEventListener "keydown" controls/game-key-down! true)
+    (js/addEventListener "keyup"   controls/game-key-up!   true)))
